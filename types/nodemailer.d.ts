bb528c7b300135a29e2e5d92845f96e038dcc7c4
srikar-k3 <srkandulapati@gmail.com>
2026-01-26 14:10:16 -0500

    bug fixes

---
+++ types/nodemailer.d.ts
@@ -0,0 +1,15 @@
+// Minimal stub to satisfy TypeScript during build on hosts
+// where @types/nodemailer isn't installed. We only import the
+// default export and call createTransport in server code.
+
+declare module 'nodemailer' {
+  interface Transporter {
+    sendMail(options: any): Promise<any>;
+  }
+  export function createTransport(options: any): Transporter;
+  const _default: {
+    createTransport: typeof createTransport;
+  };
+  export default _default;
+}
+
